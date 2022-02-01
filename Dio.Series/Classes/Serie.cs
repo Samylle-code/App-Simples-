f20341d753e9166453023f10e44281102e92cad3
Samylle <samyllecaetano@hotmail.com>
2022-01-31 23:15:50 -0300

    Felicidade

---
+++ Dio.Series/Classes/Serie.cs
@@ -0,0 +1,50 @@
+
+namespace Dio.Series.Classes
+{
+    public class Serie : EntidadeBase
+    {
+        public Genero Genero { get; set; }      
+        public string Titulo { get; set; }      
+        public string Descricao { get; set; }  
+        public int Ano { get; set; }  
+        private bool Excluido { get; set; }
+        public Serie(int id, Genero genero, string titulo, string descricao, int ano)
+        {
+            this.Id = id;
+            this.Genero = genero;
+            this.Titulo = titulo;
+            this.Descricao = descricao;
+            this.Ano = ano;
+            this.Excluido = false;
+        }
+
+        public override string ToString()
+        {
+            string retorno = "";
+            retorno += "Genero: " + this.Genero + Environment.NewLine;
+            retorno += "Titulo: " + this.Titulo + Environment.NewLine;
+            retorno += "Descrição: " + this.Descricao + Environment.NewLine;
+            retorno += "Ano de Início: " + this.Genero + Environment.NewLine;
+
+            return retorno;
+        }
+        public string retornaTitulo()
+        {
+            return this.Titulo;
+
+        }
+
+        internal int retornaId(){
+            return this.Id;
+        }
+
+        public void Excluir()
+        {
+        this.Excluido = true; 
+        }
+    }
+    
+   
+}
+
+    
